eb0c511caa2caa95e1e75579cdbdcbb93de9c3b3
Memo5561 <mehmetkose2807@gmail.com>
2026-03-18 11:27:13 +0100

    Aktualisieren von layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,32 +1,32 @@
-import "../globals.css";
 import {NextIntlClientProvider} from "next-intl";
-import {getMessages} from "next-intl/server";
+import {getMessages, setRequestLocale} from "next-intl/server";
+import {hasLocale} from "next-intl";
+import {notFound} from "next/navigation";
+import {routing} from "../../i18n/routing";
 import Navbar from "../components/navbar";
 
-export default async function LocaleLayout({
-  children,
-  params
-}: {
+type Props = {
   children: React.ReactNode;
-  params: {locale: string};
-}) {
+  params: Promise<{locale: string}>;
+};
+
+export default async function LocaleLayout({children, params}: Props) {
+  const {locale} = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
   const messages = await getMessages();
 
   return (
-    <html lang={params.locale}>
-      <body className="bg-black text-white">
-        <NextIntlClientProvider messages={messages}>
-          
-          {/* NAVBAR */}
-          <Navbar />
-
-          {/* MAIN CONTENT */}
-          <main className="mx-auto max-w-7xl px-4 py-6 pb-28 sm:px-6 sm:py-8 md:pb-8">
-            {children}
-          </main>
-
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <Navbar />
+      <main className="mx-auto max-w-7xl px-4 py-6 pb-28 sm:px-6 sm:py-8 md:pb-8">
+        {children}
+      </main>
+    </NextIntlClientProvider>
   );
 }
